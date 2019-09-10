9ac132cf3645dadbc2f3227b4dbdff0dec560d06
RD-13 <RD-13@DESKTOP-15MJ5PT>
2019-09-10 19:29:46 +0800

    1

---
+++ src/testse/testString.java
@@ -0,0 +1,12 @@
+package testse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class testString {
+	public static void main(String[] args) {
+		List<String> values = new ArrayList<>();
+		values.add("-1");
+		System.out.println(values.toArray());
+	}
+}
